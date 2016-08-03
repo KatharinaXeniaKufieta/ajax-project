10c9cc6841cfd8094d698845b6c759589cd53d16
Katharina Xenia Kufieta <katharina.kufieta@gmail.com>
2016-08-03 10:15:38 -0400

    feat: Add Google Maps API

--- js/script.js
+++ js/script.js
@@ -1,21 +1,30 @@
 
 function loadData() {
 
-    var $body = $('body');
-    var $wikiElem = $('#wikipedia-links');
-    var $nytHeaderElem = $('#nytimes-header');
-    var $nytElem = $('#nytimes-articles');
-    var $greeting = $('#greeting');
+  var $body = $('body');
+  var $wikiElem = $('#wikipedia-links');
+  var $nytHeaderElem = $('#nytimes-header');
+  var $nytElem = $('#nytimes-articles');
+  var $greeting = $('#greeting');
 
-    // clear out old data before new request
-    $wikiElem.text("");
-    $nytElem.text("");
+  // clear out old data before new request
+  $wikiElem.text('');
+  $nytElem.text('');
 
-    // load streetview
+  // get the data from the form
+  var $street = $('#street').val();
+  var $city = $('#city').val();
+  var address = $street + ', ' + $city;
 
-    // YOUR CODE GOES HERE!
+  $greeting.text('So, you want to live at ' + address + '?');
 
-    return false;
+  var link = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address;
+
+  // load streetview
+  console.log($wikiElem);
+  $body.append('<img class="bgimg" src=' + link + '>');
+
+  return false;
 };
 
 $('#form-container').submit(loadData);
