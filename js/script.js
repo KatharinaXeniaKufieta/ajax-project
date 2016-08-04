5a4f3bde32e31b78fc192daf49eb3c8ec62d8a27
Katharina Xenia Kufieta <katharina.kufieta@gmail.com>
2016-08-04 16:47:40 -0400

    feat: Add NYTimes articles to page

--- js/script.js
+++ js/script.js
@@ -21,8 +21,30 @@ function loadData() {
   var link = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address;
 
   // load streetview
-  console.log($wikiElem);
   $body.append('<img class="bgimg" src=' + link + '>');
+
+  // NYTimes AJAX request
+  var url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
+  url += '?' + $.param({
+    'api-key': "0a325e9572f4446abdb9e2075802402b",
+    'q': $city
+  });
+  $.getJSON(url, function(data) {
+    console.log(data.status);
+    console.log(data.response.docs);
+    var link, text;
+    $.each(data.response.docs, function() {
+      link = '<a href="' + this.web_url + '">' + this.headline.main + '</a>';
+      if (this.abstract === null) {
+        text = '';
+      } else {
+        text = '<p>' + this.abstract + '</p>';
+      }
+      $nytElem.append('<li class="article">' + link + text + '</li>')
+    });
+  }).error(function() {
+      $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
+    });
 
   return false;
 };
